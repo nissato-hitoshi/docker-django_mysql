acd867d8c368d7a0af86aeea693b5e5bef84012a
nissato_h <nissato_h@japacom.co.jp>
2022-05-11 09:52:16 +0900

    SQLファイルを画面に表示できるように機能修正

--- src/base/sql/home.sql
+++ src/base/sql/home.sql
@@ -1,6 +1,5 @@
 select
     v1.accounting_period
-    ,v1.accounting_month
     ,v1.budget_total
     ,IFNULL(v2.achievement_total, 0) achievement_total
     ,IFNULL(v2.achievement_total, 0) - v1.budget_total diff_total
@@ -8,23 +7,20 @@ from
     (
         select 
             accounting_period
-            ,accounting_month
             ,sum(sales_budget.sales_budget) budget_total
         from sales_budget 
         where
             version = 1 
-            group by accounting_period, accounting_month
+            group by accounting_period
     ) v1 left outer join (
         select 
             accounting_period
-            ,accounting_month
             ,sum(sales_achievement.amount_of_sales) achievement_total 
         from
             sales_achievement 
         group by
-            accounting_period, accounting_month
+            accounting_period
     ) v2 on 
-        v1.accounting_period = v2.accounting_period and
-        v1.accounting_month = v2.accounting_month
+        v1.accounting_period = v2.accounting_period
 order by
-    v1.accounting_period, v1.accounting_month
+    v1.accounting_period
