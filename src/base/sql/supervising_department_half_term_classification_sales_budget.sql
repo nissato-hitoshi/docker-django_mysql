ca5d724f1b91c0eff8cd119bb648a1b760f45c01
arasawa_m <arasawa_m@japacom.co.jp>
2022-05-12 15:19:02 +0900

    事業部ー半期ー分類別　売上予算　SQL修正

--- src/base/sql/supervising_department_half_term_classification_sales_budget.sql
+++ src/base/sql/supervising_department_half_term_classification_sales_budget.sql
@@ -1,182 +1,63 @@
 SELECT
-  AA.supervising_department
-  ,BB.first_half_Employee_total
-  ,BB.first_half_outsourcing_total
-  ,BB.first_half_product_sales_total
-  ,BB.first_half_sales_budget_total
-  ,CC.second_half_Employee_total
-  ,CC.second_half_outsourcing_total
-  ,CC.second_half_product_sales_total
-  ,CC.second_half_sales_budget_total
-  ,AA.full_year_Employee_total
-  ,AA.full_year_outsourcing_total
-  ,AA.full_year_product_sales_total
-  ,AA.full_year_sales_budget_total
+  accounting_period AS 会計期
+  ,supervising_department AS 事業部
+  ,SUM(
+      CASE WHEN half_period = "1:上期" AND
+      classification = "1:社員" THEN sales_budget
+      ELSE 0 END 
+  ) AS 上期：社員
+  ,SUM(
+      CASE WHEN half_period = "1:上期" AND
+      classification = "2:外注" THEN sales_budget
+      ELSE 0 END 
+  ) AS 上期：外注
+  ,SUM(
+      CASE WHEN half_period = "1:上期" AND
+      classification = "3:物販" THEN sales_budget
+      ELSE 0 END 
+  ) AS 上期：物販
+  ,SUM(
+      CASE WHEN half_period = "1:上期" THEN sales_budget
+      ELSE 0 END 
+  ) AS 上期：合計
+  ,SUM(
+      CASE WHEN half_period = "2:下期" AND
+      classification = "1:社員" THEN sales_budget
+      ELSE 0 END 
+  ) AS 下期：社員
+  ,SUM(
+      CASE WHEN half_period = "2:下期" AND
+      classification = "2:外注" THEN sales_budget
+      ELSE 0 END 
+  ) AS 下期：外注
+  ,SUM(
+      CASE WHEN half_period = "2:下期" AND
+      classification = "3:物販" THEN sales_budget
+      ELSE 0 END 
+  ) AS 下期：物販
+  ,SUM(
+      CASE WHEN half_period = "2:下期" THEN sales_budget
+      ELSE 0 END
+  ) AS 下期：合計
+  ,SUM(
+      CASE WHEN classification = "1:社員" THEN sales_budget
+      ELSE 0 END
+  ) AS 通期：社員
+  ,SUM(
+      CASE WHEN classification = "2:外注" THEN sales_budget
+      ELSE 0 END
+  ) AS 通期：外注
+  ,SUM(
+      CASE WHEN classification = "3:物販" THEN sales_budget
+      ELSE 0 END
+  ) AS 通期：物販
+  ,SUM(sales_budget) AS 通期：合計
 FROM
-  (                                                                        --通期
-    SELECT
-      A.supervising_department
-      ,IFNULL(B.full_year_Employee_total,0) full_year_Employee_total
-      ,C.full_year_outsourcing_total
-      ,IFNULL(D.full_year_product_sales_total,0) full_year_product_sales_total
-      ,A.full_year_sales_budget_total
-    FROM
-      (
-        SELECT
-          supervising_department
-          ,sum(sales_budget) full_year_sales_budget_total
-        FROM
-          sales_budget
-        GROUP BY
-          supervising_department
-      ) A
-      LEFT JOIN (
-        SELECT
-          supervising_department
-          ,sum(sales_budget) full_year_Employee_total
-        FROM
-          sales_budget
-        WHERE
-          classification = "1:社員"
-        GROUP BY
-          supervising_department
-      ) B ON A.supervising_department = B.supervising_department
-      LEFT JOIN (
-        SELECT
-          supervising_department
-          ,sum(sales_budget) full_year_outsourcing_total
-        FROM
-          sales_budget
-        WHERE
-          classification = "2:外注"
-        GROUP BY
-          supervising_department
-      ) C ON A.supervising_department = C.supervising_department
-      LEFT JOIN (
-        SELECT
-          supervising_department
-          ,sum(sales_budget) full_year_product_sales_total
-        FROM
-          sales_budget
-        WHERE
-          classification = "3:物販"
-        GROUP BY
-          supervising_department
-      ) D ON A.supervising_department = D.supervising_department
-  ) AA
-  LEFT JOIN                                                          
-  (                                                                  --上期
-    SELECT
-      A.supervising_department
-      ,IFNULL(B.first_half_Employee_total,0) first_half_Employee_total
-      ,C.first_half_outsourcing_total
-      ,IFNULL(D.first_half_product_sales_total,0) first_half_product_sales_total
-      ,A.first_half_sales_budget_total
-    FROM
-      (
-        SELECT
-          supervising_department
-          ,sum(sales_budget) first_half_sales_budget_total
-        FROM
-          sales_budget
-        WHERE
-          half_period = "1:上期"
-        GROUP BY
-          supervising_department
-      ) A
-      LEFT JOIN (
-        SELECT
-          supervising_department
-          ,sum(sales_budget) first_half_Employee_total
-        FROM
-          sales_budget
-        WHERE
-          half_period = "1:上期"
-          AND classification = "1:社員"
-        GROUP BY
-          supervising_department
-      ) B ON A.supervising_department = B.supervising_department
-      LEFT JOIN (
-        SELECT
-          supervising_department
-          ,sum(sales_budget) first_half_outsourcing_total
-        FROM
-          sales_budget
-        WHERE
-          half_period = "1:上期"
-          AND classification = "2:外注"
-        GROUP BY
-          supervising_department
-      ) C ON A.supervising_department = C.supervising_department
-      LEFT JOIN (
-        SELECT
-          supervising_department
-          ,sum(sales_budget) first_half_product_sales_total
-        FROM
-          sales_budget
-        WHERE
-          half_period = "1:上期"
-          AND classification = "3:物販"
-        GROUP BY
-          supervising_department
-      ) D ON A.supervising_department = D.supervising_department
-  ) BB ON AA.supervising_department = BB.supervising_department
-  LEFT JOIN                                                                 
-  (                                                                         --下期 
-    SELECT
-      A.supervising_department
-      ,IFNULL(B.second_half_Employee_total,0) second_half_Employee_total
-      ,C.second_half_outsourcing_total
-      ,IFNULL(D.second_half_product_sales_total, 0) second_half_product_sales_total
-      ,A.second_half_sales_budget_total
-    FROM
-      (
-        SELECT
-          supervising_department
-          ,sum(sales_budget) second_half_sales_budget_total
-        FROM
-          sales_budget
-        WHERE
-          half_period = "2:下期"
-        GROUP BY
-          supervising_department
-      ) A
-      LEFT JOIN (
-        SELECT
-          supervising_department
-          ,sum(sales_budget) second_half_Employee_total
-        FROM
-          sales_budget
-        WHERE
-          half_period = "2:下期"
-          AND classification = "1:社員"
-        GROUP BY
-          supervising_department
-      ) B ON A.supervising_department = B.supervising_department
-      LEFT JOIN (
-        SELECT
-          supervising_department
-          ,sum(sales_budget) second_half_outsourcing_total
-        FROM
-          sales_budget
-        WHERE
-          half_period = "2:下期"
-          AND classification = "2:外注"
-        GROUP BY
-          supervising_department
-      ) C ON A.supervising_department = C.supervising_department
-      LEFT JOIN (
-        SELECT
-          supervising_department
-          ,sum(sales_budget) second_half_product_sales_total
-        FROM
-          sales_budget
-        WHERE
-          half_period = "2:下期"
-          AND classification = "3:物販"
-        GROUP BY
-          supervising_department
-      ) D ON A.supervising_department = D.supervising_department
-  ) CC ON AA.supervising_department = CC.supervising_department
+  sales_budget
+WHERE
+  accounting_period = 43
+GROUP BY
+  accounting_period
+  ,supervising_department
 ORDER BY
-  AA.full_year_sales_budget_total desc;
+  通期：合計 DESC;
